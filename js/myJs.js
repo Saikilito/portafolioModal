3f9f8d1aaf1721e35742bee57fd38a0ec04ab869
Saikilito <kembert@gmail.com>
2018-12-23 13:10:23 -0400

    Commit inicial

---
+++ js/myJs.js
@@ -0,0 +1,16 @@
+(
+    (d) =>{
+        d.addEventListener('DOMContentLoaded', e => {
+            const copy = d.querySelector('.Footer small')
+
+            copy.innerHTML = `&copy; ${new Date().getFullYear()} @Saikilito`;
+
+            $('.navbar-nav>li>a').on('click',()=>$('.navbar-collapse').collapse('hide'));
+
+            const scroll = new SmoothScroll('a[href*="#"]', {
+                offset: 50,
+                selectorHeader: '.navbar'
+              })
+        })
+}
+)(document);
